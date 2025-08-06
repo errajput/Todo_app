12cdc92789ba1022dcbd84117678fe85f496ac5d
disha rajput <dishacsevira@gmail.com>
2025-08-06 16:43:22 +0530

    create component of checkbox

--- src/components/CustomCheckbox.jsx
+++ src/components/CustomCheckbox.jsx
@@ -1,7 +1,12 @@
 const CustomCheckbox = ({ onChange, checked }) => {
   return (
     <label className="relative cursor-pointer">
-      <input type="checkbox" onChange={onChange} checked={checked} />
+      <input
+        type="checkbox"
+        onChange={onChange}
+        checked={checked}
+        className=""
+      />
     </label>
   );
 };
