5db0030a786cabebf40bfeb768ddf4d87a27c493
disha rajput <dishacsevira@gmail.com>
2025-08-07 08:50:58 +0530

    create Input container component

---
+++ src/components/InputContainer.jsx
@@ -0,0 +1,25 @@
+const InputContainer = ({ inputValue, setInputValue, handleAdd }) => {
+  return (
+    <div className="flex m-4 gap-2 justify-center mb-4">
+      <input
+        type="text"
+        className="border border-black rounded-md outline-none focus:ring-1 bg-white"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleAdd();
+          }
+        }}
+      />
+      <button
+        className="bg-gray-100  px-2 border rounded-md hover:bg-gray-50 cursor-pointer transition"
+        onClick={handleAdd}
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default InputContainer;
